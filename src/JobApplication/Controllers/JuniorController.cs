1f3b43bfd762f0ec6550b3fcbcb6ed33a32f7902
Petar Kekez <petar.kekez@outlook.com>
2016-09-22 10:44:35 +0200

    test commit

--- src/JobApplication/Controllers/JuniorController.cs
+++ src/JobApplication/Controllers/JuniorController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 using JobApplication.Database.IReporistory;
 using Microsoft.Extensions.Logging;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace JobApplication.Controllers
 {
